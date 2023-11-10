c301bfa72ccc2d81729bdd79d46d3b7110e831f4
lcgeneralprojects <lcgeneralprojects@gmail.com>
2023-11-10 14:54:24 +0500

    Added the ability to choose file extensions of files that are created using the file manager Changed several functions' signatures to ask for **kwargs

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 # This is a project for an automated file renamer
 # TODO: make the empty-prefixed calls identity operations for those base directories
 # TODO: make choice of extension available
+# TODO: consider file name + extension limitations
 
 import os
 # from exceptions import EmptyArgsException
@@ -16,9 +17,22 @@ def validate_prefix(prefix):
     return ''.join((char for char in prefix if char not in ' _0123456789'))  # Deleting unwanted characters
 
 
-def validate_args(**args):
+DEFAULT_EXTENSION = '.py'
+
+
+def validate_extension(extension):
+    if extension == '':
+        return DEFAULT_EXTENSION
+    else:
+        extension = ''.join((char for char in extension if char not in ' _0123456789'))  # Deleting unwanted characters
+        if extension[0] != '.':
+            extension = '.' + extension
+        return extension
+
+
+def validate_args(**kwargs):
     empty_args = []
-    for key, value in args.items():
+    for key, value in kwargs.items():
         if value == '' or value is None:
             empty_args.append(key)
     if empty_args:
@@ -48,7 +62,8 @@ def find_end_of_exercise_number_from_directory(directory):
     return os.path.basename(directory).split('_')[1]
 
 
-def main_file_name(directory, prefix=''):
+def main_file_name(**kwargs):   # directory, prefix=''
+    directory, prefix = kwargs['directory'], kwargs['prefix']
     res = os.path.basename(directory)
     pos = find_end_of_prefix(res) + 1
     res = res[:pos] + 'f_' + res[pos:]
@@ -58,7 +73,8 @@ def main_file_name(directory, prefix=''):
     return res
 
 
-def renamer(base_dir, prefix, problem_name=None):
+def renamer(**kwargs):   # base_dir, prefix
+    base_dir, prefix = kwargs['base_dir'], kwargs['prefix']
     prefix = validate_prefix(prefix)
     for directory in os.listdir(base_dir):
         prefix_end = find_end_of_prefix(directory) + 1
@@ -96,7 +112,7 @@ def renamer(base_dir, prefix, problem_name=None):
 
                             pos_1 = file_data.find('from') + 5
                             pos_2 = file_data.find('import', pos_1) - 1
-                            file_data = file_data[:pos_1] + main_file_name(directory) + file_data[pos_2:]
+                            file_data = file_data[:pos_1] + main_file_name(directory=directory) + file_data[pos_2:]
 
                             with open(directory + '/' + file, 'w') as f:
                                 f.write(file_data)
@@ -117,7 +133,8 @@ def renamer(base_dir, prefix, problem_name=None):
 
 
 # TODO: unify this with the renamer function
-def imp_adjustment(base_dir, prefix=None, problem_name=None):
+def imp_adjustment(**kwargs):   # base_dir
+    base_dir = kwargs['base_dir']
     for directory in os.listdir(base_dir):
         directory = base_dir + '/' + directory
         if os.path.isdir(directory):
@@ -153,7 +170,7 @@ def imp_adjustment(base_dir, prefix=None, problem_name=None):
 
 # The function for transforming names of problems into appropriate file names
 # TODO: currently, only supports Leetcode. Need to make it more general.
-def get_file_name(problem_name):
+def get_file_name(problem_name):    # problem_name
     trans_dict = str.maketrans(' ', '_', '.')
     res = problem_name.translate(trans_dict).lower()
     res = res[:res.find('_') + 1] + 'f_' + res[res.find('_') + 1:]
@@ -161,13 +178,16 @@ def get_file_name(problem_name):
 
 
 # TODO: check for errors using a decorator?
-def file_creation(base_dir, prefix, problem_name):
+# TODO: allow for configurable file extensions
+def file_creation(**kwargs):        # base_dir, prefix, problem_name
+    base_dir, prefix, problem_name, extension = kwargs['base_dir'], kwargs['prefix'], kwargs['problem_name'], kwargs['extension']
     try:
         validate_args(base_dir=base_dir, prefix=prefix, problem_name=problem_name)
     except EmptyArgsException:
         return
 
     prefix = validate_prefix(prefix)
+    extension = validate_extension(extension)
 
     main_file_name = prefix + '_' + get_file_name(problem_name)
     directory_name = os.path.join(base_dir, main_file_name.replace('f_', ''))
@@ -177,14 +197,14 @@ def file_creation(base_dir, prefix, problem_name):
         os.mkdir(directory_name)
 
     try:
-        with open(directory_name + '/' + main_file_name + '.py', 'x') as f:
+        with open(directory_name + '/' + main_file_name + extension, 'x') as f:
             pass
     except FileExistsError:
         pass
 
-    with open(directory_name + '/' + test_file_name + '.py', 'w') as new_test_file:
+    with open(directory_name + '/' + test_file_name + extension, 'w') as new_test_file:
         try:
-            with open(base_dir + f'/{prefix}_common/{prefix}_test_template.py', 'r') as template:
+            with open(base_dir + f'/{prefix}_common/{prefix}_test_template' + extension, 'r') as template:
                 for line in template:
                     new_test_file.write(line)
         except FileNotFoundError:
@@ -194,8 +214,10 @@ def file_creation(base_dir, prefix, problem_name):
 METHODS_DICT = {'renamer': renamer, 'import_adjustment': imp_adjustment, 'file_creation': file_creation}
 
 
-def general_function_handler(method, base_dir, prefix, problem_name):
-    METHODS_DICT[method](base_dir, prefix, problem_name)
+def general_function_handler(**kwargs): # method, base_dir, prefix, problem_name
+    method = kwargs['method']
+    del kwargs['method']
+    METHODS_DICT[method](**kwargs)
 
 
 if __name__ == '__main__':
@@ -209,12 +231,12 @@ if __name__ == '__main__':
         if method == 'renamer':
             base_dir = str(input("Base directory: "))
             prefix = str(input("Prefix: "))
-            renamer(base_dir, prefix)
+            renamer(base_dir=base_dir, prefix=prefix)
         elif method == 'import_adjustment':
             base_dir = str(input("Base directory: "))
-            imp_adjustment(base_dir)
+            imp_adjustment(base_dir=base_dir)
         elif method == 'file_creation':
             base_dir = str(input("Base directory: "))
             prefix = str(input("Prefix: "))
             problem_name = str(input("Problem name: "))
-            file_creation(base_dir, prefix, problem_name)
+            file_creation(base_dir=base_dir, prefix=prefix, problem_name=problem_name)
--- main.py
+++ main.py
@@ -25,9 +25,9 @@ label_and_entry_frame = tk.Frame(root, relief='raised', borderwidth=5)  # TODO: 
 # label_and_entry_frame.grid(row=0, column=0, sticky='nsew')
 label_and_entry_frame.pack(padx=20, pady=10, fill='x')
 for c in range(4): label_and_entry_frame.columnconfigure(index=c, weight=1)
-for r in range(6): label_and_entry_frame.rowconfigure(index=r, weight=1)
+for r in range(7): label_and_entry_frame.rowconfigure(index=r, weight=1)
 label_and_entry_frame.columnconfigure(index=1, weight=2, minsize=200)
-label_and_entry_frame.rowconfigure(index=4, minsize=16)
+label_and_entry_frame.rowconfigure(index=5, minsize=16)
 # # Preset frame
 # preset_frame = tk.Frame(main_body_frame)
 # preset_frame.grid(row=1, column=0, sticky='nsew')
@@ -45,8 +45,10 @@ prefix_label = tk.Label(label_and_entry_frame, text='Prefix: ')
 prefix_label.grid(row=2, column=0, sticky='E')
 problem_name_label = tk.Label(label_and_entry_frame, text='Problem name: ')
 problem_name_label.grid(row=3, column=0, sticky='E')
+extension_label = tk.Label(label_and_entry_frame, text='Extension: ')
+extension_label.grid(row=4, column=0, sticky='E')
 preset_name_label = tk.Label(label_and_entry_frame, text='Preset name: ')
-preset_name_label.grid(row=5, column=0, sticky='E')
+preset_name_label.grid(row=6, column=0, sticky='E')
 
 # Entry section
 # TODO: make method_entry a drop-down menu
@@ -58,16 +60,19 @@ prefix_entry = tk.Entry(label_and_entry_frame)
 prefix_entry.grid(row=2, column=1, sticky='EW')
 problem_name_entry = tk.Entry(label_and_entry_frame)
 problem_name_entry.grid(row=3, column=1, sticky='EW')
+extension_entry = tk.Entry(label_and_entry_frame)
+extension_entry.grid(row=4, column=1, sticky='EW')
 preset_name_entry = tk.Entry(label_and_entry_frame)
-preset_name_entry.grid(row=5, column=1, sticky='EW')
+preset_name_entry.grid(row=6, column=1, sticky='EW')
 
 ENTRY_OBJECT_DICT = {'method': method_entry, 'base dir': base_dir_entry, 'prefix': prefix_entry,
-                     'problem name': problem_name_entry, 'preset name': preset_name_entry}
+                     'problem name': problem_name_entry,'extension': extension_entry, 'preset name': preset_name_entry}
 
 # Button section
 ok_button = tk.Button(bottom_frame, text='Ok',
-                      command=lambda: general_function_handler(method_entry.get(), base_dir_entry.get(),
-                                                               prefix_entry.get(), problem_name_entry.get()))
+                      command=lambda: general_function_handler(method=method_entry.get(), base_dir=base_dir_entry.get(),
+                                                               prefix=prefix_entry.get(), problem_name=problem_name_entry.get(),
+                                                               extension=extension_entry.get()))
 ok_button.pack()
 
 def set_base_dir():
@@ -111,9 +116,10 @@ def save_preset():
                 file.write(key + ': ' + value + '\n')
 
 save_preset_button = tk.Button(label_and_entry_frame, text='save preset', command=save_preset)
-save_preset_button.grid(row=5, column=2, sticky='EW')
+save_preset_button.grid(row=6, column=2, sticky='EW')
 
-
+# TODO: handle the FileNotFoundError when the directory request gets cancelled more gracefully
+# TODO: add default preset directory
 def choose_preset():
     filename = filedialog.askopenfilename()
     param_dict = {}
@@ -132,7 +138,7 @@ def choose_preset():
             ENTRY_OBJECT_DICT[key].insert(0, value)
 
 choose_preset_button = tk.Button(label_and_entry_frame, text='choose preset', command=choose_preset)
-choose_preset_button.grid(row=5, column=3, sticky='EW')
+choose_preset_button.grid(row=6, column=3, sticky='EW')
 
 
 if __name__ == '__main__':
